04c9cc674554c55ca09ca939b0895075215aeb5d
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-06 20:39:58 -0300

    test: ensure SignUpController returns 400

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from "./signup";
+
+describe("Sign Up Controller", () => {
+  it("Should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        // name:"any_name", removing name for the test
+        email: "email@mail.com",
+        password: "any_pass",
+        passwordConfirmation: "any_pass",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
