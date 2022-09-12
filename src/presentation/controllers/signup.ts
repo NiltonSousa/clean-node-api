a4e7f748036434c789395a4e54312453c8aace0c
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-12 20:37:47 -0300

    refactor: create loop to validate required fields

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badResquest(new MissingParamError("name"));
-    }
-    if (!httpRequest.body.email) {
-      return badResquest(new MissingParamError("email"));
+    const requiredFields = ["name", "email"];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badResquest(new MissingParamError(field));
+      }
     }
   }
 }
