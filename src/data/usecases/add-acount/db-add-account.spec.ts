e4c61e7a8b40a88d315018b361c3cd9c536bc9ec
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-23 21:35:03 -0300

    test: ensure AddAccountRepository throw when throws

--- src/data/usecases/add-acount/db-add-account.spec.ts
+++ src/data/usecases/add-acount/db-add-account.spec.ts
@@ -82,6 +82,26 @@ describe("DbAddAccount Usecase", () => {
     await expect(promise).rejects.toThrow();
   });
 
+  it("Should throw if AddACcountRepository throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_mail@mail.com",
+      password: "valid_pass",
+    };
+
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
+  });
+
   it("Should call AddAccountRepository with correct data", async () => {
     const { sut, addAccountRepositoryStub } = makeSut();
 
