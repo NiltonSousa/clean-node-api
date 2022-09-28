898836142932bcce2cfa868887266b8d6431acc0
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-28 07:37:36 -0300

    test: ensure BCryptAdapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from "bcrypt";
 import { BCryptAdapter } from "./bcrypt-adapter";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
 describe("BCrypt Adapter", () => {
   it("Should call bcrypt with correct value", async () => {
     const sut = new BCryptAdapter(12);
@@ -8,5 +14,12 @@ describe("BCrypt Adapter", () => {
     await sut.encrypt("any_value");
 
     expect(hashSpy).toHaveBeenCalledWith("any_value", 12);
+  });
+
+  it("Should call a hash on success", async () => {
+    const sut = new BCryptAdapter(12);
+    const hash = await sut.encrypt("any_value");
+
+    expect(hash).toBe("hash");
   });
 });
