037a0125cd6f014a95a4cafc627fb367ac6d74b8
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-10-26 12:49:02 -0300

    test: ensure request response body receive when request success

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -3,12 +3,12 @@ import request from "supertest";
 import app from "../config/app";
 
 describe("Body parser middleware", () => {
-  it("", async () => {
-    app.post("test_body_parser", (req, res) => {
+  it("Should return response body when request success", async () => {
+    app.post("/test_body_parser", (req, res) => {
       res.send(req.body);
     });
 
-    const test = await request(app)
+    await request(app)
       .post("/test_body_parser")
       .send({ name: "nil" })
       .expect({ name: "nil" });
